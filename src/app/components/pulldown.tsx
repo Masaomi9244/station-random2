9f2c5f8e9417dd7f27eacdde77abcbe02981adb8
Masaomi9244 <mario8masaomiomi@gmail.com>
2024-11-09 21:35:13 +0900

    fix-space-and-add-last-blank

--- src/app/components/pulldown.tsx
+++ src/app/components/pulldown.tsx
@@ -4,17 +4,17 @@ import dynamic from "next/dynamic";
 const CustomSelectComponent = dynamic(() => import("./customSelect"), { ssr: false });
 
 interface PullDownProps {
-	label: string;
-	values: string[];
+  label: string;
+  values: string[];
 }
 
 export default function PullDown(props: PullDownProps) {
-	return (
-		<Box sx={{ marginLeft: '16px', width: '150px' }}>
-			<Typography color='textDisabled'>{props.label}</Typography>
-			<FormControl variant="standard">
-				<CustomSelectComponent values={props.values} />
-			</FormControl>
-		</Box>
-	)
+  return (
+    <Box sx={{ marginLeft: '16px', width: '150px' }}>
+      <Typography color='textDisabled'>{props.label}</Typography>
+      <FormControl variant="standard">
+        <CustomSelectComponent values={props.values} />
+      </FormControl>
+    </Box>
+  )
 }
