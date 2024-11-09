e1bad42a558dc3e3aaede557bfad2fb9b95f1c52
Masaomi9244 <mario8masaomiomi@gmail.com>
2024-11-09 21:28:54 +0900

    add-select-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-
-import { Button } from "@mui/material"
+import { Box, Button, FormControl, Typography } from "@mui/material"
+import PullDown from "./components/pulldown";
 
 export default function Home() {
+  const areas = ['関東', '東北'];
+  const prefectures = ['神奈川県', '東京都', '埼玉県', '千葉県',];
+  const lines = ['JR横浜線', '京浜急行本線', 'JR中央線', '小田急小田原線']
+
   return (
-    <>
-      <Button variant="contained">ボタン</Button>
-    </>
-  )
+    <Box sx={{ display: 'flex', flexFlow: 'column', alignItems: 'center' }}>
+      <Typography sx={{ marginBottom: '16px', marginRight: '320px', fontSize: '32px' }}>StationRandom</Typography>
+      <Box sx={{ display: 'flex', alignItems: 'end' }}>
+        <PullDown label={'地方'} values={areas}></PullDown>
+        <PullDown label={'都道府県'} values={prefectures}></PullDown>
+        <PullDown label={'路線'} values={lines}></PullDown>
+        <Button variant="contained" sx={{ marginLeft: '16px' }}>検索</Button>
+      </Box>
+    </Box>
+  );
 }
